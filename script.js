ec1d2dd630cec36639bfd27d7c16de4308b5ff31
Dan Lennon <safarileader@gmail.com>
2017-05-27 10:56:00 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+
+	$('div.buttons').on('click','.btn',function(e){
+
+		if ($(e.target).hasClass('btn-danger')){
+			alert('Correct!\nThat\'s Not Jollof!');
+
+			let $score = $('.correct-answer');
+			$score.text(parseInt($score.text())+1);
+
+		}
+
+		if ($(e.target).hasClass('btn-success')){
+			alert('Incorrect!\nThat\'s Not Jollof');
+
+			let $score = $('.incorrect-answer');
+			$score.text(parseInt($score.text())+1);
+		}
+
+		//reload the image!
+		$("img").attr("src", "http://lorempixel.com/400/200/food/")
+	});
+
+});
